b09a28a1c8929a3bbc74aa355cb3802e2d483479
Rajeshwari-21 <rajeshwari210999@gmail.com>
2024-06-30 17:52:43 +0530

    prediction pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -23,8 +23,10 @@ class DataTransformation:
         self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
+        '''
+        This function si responsible for data trnasformation
         
-        
+        '''
         try:
             numerical_columns = ["writing_score", "reading_score"]
             categorical_columns = [
@@ -85,6 +87,7 @@ class DataTransformation:
 
             target_column_name="math_score"
             numerical_columns = ["writing_score", "reading_score"]
+            
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
